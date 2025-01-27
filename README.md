d9f06dde58c3968cd5158c6c498f8e62eb596e9b
Jeff Merckens <jeff.merckens@gmail.com>
2025-01-27 06:50:40 -0500

    feat: add getAffordableSymbols to find potential stock buys

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ bun run index.ts
 ```
 
 This project was created using `bun init` in bun v1.2.0. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+## Notes
+
+WIP - Currently working on this.
+
+- Pulls the Alpaca API key and secret from environment variables; eventually these will be set in a project on Railway.com (where I'll be hosting it), but for development purposes one can just make/use a `.env` file, e.g. `.env.local`.
+- Eventually will have it message a Discord webhook for a simple logging/notification setup.
--- index.ts
+++ index.ts
@@ -8,34 +8,37 @@ const alpaca = new Alpaca({
   paper: true,
 });
 
-// Get all the stocks we own
-async function getPositionBars() {
-  const positions = await alpaca.getPositions();
-  console.log(positions);
-  const positionSymbols = positions.map((p: any) => p.symbol);
+// // Main function
+// const positionBars = await getPositionBars();
+// const [shortAverage, longAverage] = calculateAverages(positionBars);
+// console.log("positionAverages", shortAverage, longAverage);
 
-  // Determine if we should sell any of our stocks
-  const daysToFetch = 151;
-  const startDate = new Date();
-  startDate.setDate(startDate.getDate() - daysToFetch);
+// // Sell any stocks we should sell
+// if (longAverage === 0 || shortAverage <= longAverage) {
+//   console.log("Sell the stock");
+//   alpaca.createOrder({
+//     symbol: "AAPL",
+//     qty: 1,
+//     side: "sell",
+//     type: "market",
+//     time_in_force: "day",
+//   });
+// }
 
-  const bars = alpaca.getMultiBarsAsyncV2(positionSymbols, {
-    start: startDate.toISOString().slice(0, 10),
-    timeframe: alpaca.newTimeframe(1, alpaca.timeframeUnit.DAY),
-    limit: daysToFetch,
-  });
-  const barsReceived = [];
-  for await (let b of bars) {
-    barsReceived.push(b);
-  }
-  barsReceived.reverse();
-  console.log(barsReceived);
+const account = await alpaca.getAccount();
+const cash = account.cash;
 
-  return barsReceived;
-}
+const affordableSymbols = await getAffordableSymbols(cash);
 
-// Calculate moving averages
-function calculateAverages(stockBars: AlpacaBar[]) {
+// Determine if we should buy any of them
+// Buy any new stocks we should buy
+
+/**
+ * Calculates simple averages of a stock's closing prices over a shorter and a longer time period
+ * @param stockBars An array of bars for one stock
+ * @returns An array with the short and long averages
+ */
+function calculateAverages(stockBars: AlpacaBar[]): number[] {
   let shortAverage = 0;
   let longAverage = 0;
 
@@ -53,12 +56,72 @@ function calculateAverages(stockBars: AlpacaBar[]) {
   return [shortAverage, longAverage];
 }
 
-const positionBars = await getPositionBars();
-const positionAverages = calculateAverages(positionBars);
-console.log("positionAverages", positionAverages);
+/**
+ * Retrieves info on any stocks owned; used to determine if we should sell any
+ * @returns Bars for all the stocks we own
+ */
+async function getPositionBars(): Promise<AlpacaBar[]> {
+  // Get a list of stocks owned
+  const positions = await alpaca.getPositions();
+  console.log(positions);
+  const positionSymbols = positions.map((p: any) => p.symbol);
 
-// Sell any stocks we should sell
+  // Get price data for the stocks we own
+  const daysToFetch = 151;
+  const startDate = new Date();
+  startDate.setDate(startDate.getDate() - daysToFetch);
 
-// Determine if we should buy any new stocks
+  const bars = alpaca.getMultiBarsAsyncV2(positionSymbols, {
+    start: startDate.toISOString().slice(0, 10),
+    timeframe: alpaca.newTimeframe(1, alpaca.timeframeUnit.DAY),
+    limit: daysToFetch,
+  });
+  const barsReceived = [];
+  for await (let b of bars) {
+    barsReceived.push(b);
+  }
+  barsReceived.reverse(); // Changes the order to most recent -> oldest
 
-// Buy any new stocks we should buy
+  return barsReceived;
+}
+
+/**
+ * Retrieves a list of stocks (listed on the NASDAQ and NYSE) that are affordable given a cash balance
+ * @param cashBalance The amount of cash available to spend
+ * @returns A list of affordable stock symbols
+ */
+async function getAffordableSymbols(cashBalance: number): Promise<string[]> {
+  // Get a list of tradable stocks
+  const allAssets = await alpaca.getAssets({ status: "active" });
+  const allTradableAssets = allAssets.filter(
+    (asset: any) =>
+      ["nasdaq", "nyse"].includes(asset.exchange.toLowerCase()) &&
+      asset.tradable
+  );
+
+  // Get the most recently traded price for each stock
+  const allQuotes = await alpaca.getLatestQuotes(
+    allTradableAssets.map((a: any) => a.symbol)
+  );
+
+  // Filter out the stocks that are too expensive for us
+  const targetAssets = allQuotes
+    .entries()
+    .filter(([, quote]: [string, any]) => {
+      return (
+        quote.AskPrice < cashBalance / 10 || quote.BidPrice < cashBalance / 10
+      );
+    });
+
+  // Filter out less frequently traded stocks
+  const pastWeek = new Date();
+  pastWeek.setDate(pastWeek.getDate() - 5);
+
+  const recentTargetAssets = targetAssets.filter(
+    ([, quote]: [string, any]) => new Date(quote.Timestamp) > pastWeek
+  );
+
+  return Array.from(
+    recentTargetAssets.map(([symbol, quote]: [string, any]) => symbol)
+  );
+}
