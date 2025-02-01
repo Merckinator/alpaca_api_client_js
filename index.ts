f9fdcc986d9579868d46ba883f9e1e11a05ef287
Jeff Merckens <jeff.merckens@gmail.com>
2025-02-01 14:44:29 -0500

    feat: calculate 2 moving averages; identify buyable symbols

--- index.ts
+++ index.ts
@@ -9,7 +9,9 @@ const alpaca = new Alpaca({
 });
 
 // // Main function
-// const positionBars = await getPositionBars();
+// SECTION: Sell any stocks we should sell
+// const positionSymbols = await getPositionSymbols();
+// const positionBars = await getBars(positionSymbols);
 // const [shortAverage, longAverage] = calculateAverages(positionBars);
 // console.log("positionAverages", shortAverage, longAverage);
 
@@ -25,64 +27,122 @@ const alpaca = new Alpaca({
 //   });
 // }
 
+// SECTION: Find new stocks to buy
 const account = await alpaca.getAccount();
 const cash = account.cash;
-
 const affordableSymbols = await getAffordableSymbols(cash);
+const affordableBars = await getBars(affordableSymbols);
 
-// Determine if we should buy any of them
+const affordableAverages = affordableBars
+  .map((bars) => {
+    if (bars.length < 1) {
+      return { symbol: "NO_DATA", shortAverages: [0, 0], longAverages: [0, 0] };
+    } else {
+      const [shortAverages, longAverages] = calculateAverages(bars);
+
+      return { symbol: bars[0].Symbol, shortAverages, longAverages };
+    }
+  })
+  .filter((a) => a.symbol !== "NO_DATA");
+
+// Want to buy stocks where the short average just rose above the long average.
+// This is a bullish signal that the stock is likely starting to rise in price.
+const buyableAverages = affordableAverages.filter(
+  (a) =>
+    a.shortAverages[0] > a.longAverages[0] &&
+    a.shortAverages[1] <= a.longAverages[1]
+);
+
+console.log(buyableAverages);
+
 // Buy any new stocks we should buy
+// SECTION: Buy any new stocks that we identified
 
 /**
- * Calculates simple averages of a stock's closing prices over a shorter and a longer time period
+ * Calculates the 2 most recent moving averages of stock closing prices over a shorter and a longer time period
  * @param stockBars An array of bars for one stock
- * @returns An array with the short and long averages
+ * @returns An array of arrays of the short and long averages
  */
-function calculateAverages(stockBars: AlpacaBar[]): number[] {
-  let shortAverage = 0;
-  let longAverage = 0;
+function calculateAverages(stockBars: AlpacaBar[]): number[][] {
+  const daysInShortAverage = 30;
+  const daysInLongAverage = 100;
 
-  if (stockBars.length >= 30) {
-    const shortStockBars = stockBars.map((bar) => bar.ClosePrice).splice(0, 30);
-    shortAverage =
-      shortStockBars.reduce((x, y) => x + y) / shortStockBars.length;
+  let [shortAverage1, shortAverage2] = [0, 0];
+  let [longAverage1, longAverage2] = [0, 0];
+
+  const closingPrices = stockBars.map((bar) => bar.ClosePrice);
+
+  if (stockBars.length >= 31) {
+    const shortSum1 = closingPrices
+      .slice(0, daysInShortAverage)
+      .reduce((x, y) => x + y);
+
+    const shortSum2 = closingPrices
+      .slice(1, daysInShortAverage)
+      .reduce((x, y) => x + y);
+
+    shortAverage1 = shortSum1 / daysInShortAverage;
+    shortAverage2 = shortSum2 / daysInShortAverage;
   }
 
-  if (stockBars.length >= 100) {
-    const longStockBars = stockBars.map((bar) => bar.ClosePrice).splice(0, 100);
-    longAverage = longStockBars.reduce((x, y) => x + y) / longStockBars.length;
+  if (stockBars.length >= 101) {
+    const longSum1 = closingPrices
+      .slice(0, daysInLongAverage)
+      .reduce((x, y) => x + y);
+
+    const longSum2 = closingPrices
+      .slice(1, daysInLongAverage)
+      .reduce((x, y) => x + y);
+
+    longAverage1 = longSum1 / daysInLongAverage;
+    longAverage2 = longSum2 / daysInLongAverage;
   }
 
-  return [shortAverage, longAverage];
+  return [
+    [shortAverage1, shortAverage2],
+    [longAverage1, longAverage2],
+  ];
+}
+
+/**
+ * Gets Bars (details) for multiple stocks
+ * @param symbols An array of stock symbols
+ * @returns An array of arrays of bars for each stock, each in recent -> oldest order
+ */
+async function getBars(symbols: string[]): Promise<AlpacaBar[][]> {
+  // Get price data for the stocks we own
+  const daysToFetch = 151;
+  const startDate = new Date();
+  startDate.setDate(startDate.getDate() - daysToFetch);
+
+  const bars = alpaca.getMultiBarsAsyncV2(symbols, {
+    start: startDate.toISOString().slice(0, 10),
+    timeframe: alpaca.newTimeframe(1, alpaca.timeframeUnit.DAY),
+    limit: daysToFetch * symbols.length,
+  });
+  const barsReceived: AlpacaBar[] = [];
+  for await (let b of bars) {
+    barsReceived.push(b);
+  }
+  barsReceived.reverse(); // Changes the order to most recent -> oldest
+
+  const symbolBars: AlpacaBar[][] = [];
+  symbols.forEach((symbol) => {
+    symbolBars.push(barsReceived.filter((b) => b.Symbol === symbol));
+  });
+  return symbolBars;
 }
 
 /**
  * Retrieves info on any stocks owned; used to determine if we should sell any
  * @returns Bars for all the stocks we own
  */
-async function getPositionBars(): Promise<AlpacaBar[]> {
+async function getPositionSymbols(): Promise<string[]> {
   // Get a list of stocks owned
   const positions = await alpaca.getPositions();
   console.log(positions);
-  const positionSymbols = positions.map((p: any) => p.symbol);
 
-  // Get price data for the stocks we own
-  const daysToFetch = 151;
-  const startDate = new Date();
-  startDate.setDate(startDate.getDate() - daysToFetch);
-
-  const bars = alpaca.getMultiBarsAsyncV2(positionSymbols, {
-    start: startDate.toISOString().slice(0, 10),
-    timeframe: alpaca.newTimeframe(1, alpaca.timeframeUnit.DAY),
-    limit: daysToFetch,
-  });
-  const barsReceived = [];
-  for await (let b of bars) {
-    barsReceived.push(b);
-  }
-  barsReceived.reverse(); // Changes the order to most recent -> oldest
-
-  return barsReceived;
+  return positions.map((p: any) => p.symbol);
 }
 
 /**
